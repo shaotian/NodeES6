3abbfa8890e3fc780d9b407dc51af227ac4bd086
shaotian.ma <349943166@qq.com>
2016-07-06 09:14:02 +0800

    第十一章

--- public/javascripts/chapter11.js
+++ public/javascripts/chapter11.js
@@ -1,3 +1,15 @@
 /**
  * Created by Administrator on 2016/7/6.
  */
+var obj = new Proxy({}, {
+    get: function (target, key, receiver) {
+        console.log(`getting ${key}!`);
+        return Reflect.get(target, key, receiver);
+    },
+    set: function (target, key, value, receiver) {
+        console.log('setting ${key}');
+        return Reflect.set(target, key, value, receiver);
+    }
+});
+
+
